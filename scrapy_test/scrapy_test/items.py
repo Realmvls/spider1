753f5f6c71f96c4486aa329b9b0b0f0d6c05a590
jochen <942814208@qq.com>
2017-09-17 09:35:03 +0800

    传智播客老师信息

--- scrapy_test/scrapy_test/items.py
+++ scrapy_test/scrapy_test/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class ScrapyTestItem(scrapy.Item):
+class webtestItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
+    # pass
--- scrapy_test/scrapy_test/spiders/webtest.py
+++ scrapy_test/scrapy_test/spiders/webtest.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
-import scrapy
+#传智博客
 
+import scrapy
+from scrapy_test.items import webtestItem
 
 class WebtestSpider(scrapy.Spider):
     name = "webtest"
-    allowed_domains = ["www.taobao.com"]
-    start_urls = ['http://www.taobao.com/']
+    allowed_domains = ["www.itcast.cn"]
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml#aandrold']
 
     def parse(self, response):
-        print(response.body)
+        data_list = response.xpath("//div[@class = 'li_txt']")
+        items = []
+        for data in data_list:
+            item = webtestItem()
+            name = data.xpath("./h3/text()").extract()
+            title = data.xpath("./h4/text()").extract()
+            info = data.xpath("./p/text()").extract()
+            item['name']=name[0]
+            item['title']=title[0]
+            item['info']=info[0]
+            items.append(item)
+        return items
+        # print(response.body)
         #pass
